f9e5e5953db33db941b378539e0cf7482a7a7a51
Zsombor Jáger <zsombor.jager@stud.uni-corvinus.hu>
2022-09-30 14:51:28 +0200

    Fájlba írás

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,14 +18,16 @@ namespace WindowsFormsApp1
         BindingList<User> users = new BindingList<User>();
         public Form1()
         {
+            InitializeComponent();
+
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
 
-            InitializeComponent();
             label1.Text = Resource1.FullName;
             //label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,6 +39,29 @@ namespace WindowsFormsApp1
             };
             users.Add(u);
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var u in users)
+                {
+                    sw.Write(u.FullName);
+                    sw.Write(";");
+                    sw.Write(u.ID);
+                    sw.WriteLine();
+
+                }
+            }
         }
     }
 }
